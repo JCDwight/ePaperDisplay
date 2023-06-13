c304db62f84101c39a521ed01f745de705a68e99
Jay Dwight <jcdwight@gmail.com>
2023-06-13 01:04:37 -0400

    L

--- ePaperDisplay.py
+++ ePaperDisplay.py
@@ -24,52 +24,15 @@ try:
     logging.info("init and Clear")
     epd.init()
     epd.Clear()
-
+    font96 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 96)
+    font48 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 48)
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
     font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-    #font24 = ImageFont.truetype('pic/Font.ttc', 24)
-    #font18 = ImageFont.truetype('pic/Font.ttc', 18)
-    font48 = ImageFont.truetype('pic/Font.ttc', 48)
-
-
-    # Drawing on the Vertical image
-    logging.info("2.Drawing on the Vertical image...")
-    Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    draw = ImageDraw.Draw(Limage)
-    draw.text((2, 0), 'hello world', font = font18, fill = 0)
-    draw.text((2, 20), '7.5inch epd', font = font18, fill = 0)
-    draw.text((20, 50), u'微雪电子', font = font18, fill = 0)
-    draw.line((10, 90, 60, 140), fill = 0)
-    draw.line((60, 90, 10, 140), fill = 0)
-    draw.rectangle((10, 90, 60, 140), outline = 0)
-    draw.line((95, 90, 95, 140), fill = 0)
-    draw.line((70, 115, 120, 115), fill = 0)
-    draw.arc((70, 90, 120, 140), 0, 360, fill = 0)
-    draw.rectangle((10, 150, 60, 200), fill = 0)
-    draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
-    epd.display(epd.getbuffer(Limage))
-    time.sleep(2)
-
-    logging.info("3.read bmp file")
-    Himage = Image.open(os.path.join(picdir, '7in5_V2.bmp'))
-    epd.display(epd.getbuffer(Himage))
-    time.sleep(2)
-
-    logging.info("4.read bmp file on window")
-    Himage2 = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
-    Himage2.paste(bmp, (50,10))
-    epd.display(epd.getbuffer(Himage2))
-    time.sleep(2)
-
-    logging.info("Clear...")
-    epd.init()
-    epd.Clear()
 
     Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(Limage)
 
-    draw.text((200,100), 'Hello World', font = font48, fill = 0)
+    draw.text((0,0), 'Hello World', font = font48, fill = 0)
 
     epd.display(epd.getbuffer(Limage))
     time.sleep(2)
