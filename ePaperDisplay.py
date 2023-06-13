d699ab91d41bbcfd8cb6e0424c33c107e0e9d4b1
Jay Dwight <jcdwight@gmail.com>
2023-06-13 00:40:40 -0400

    /?

--- ePaperDisplay.py
+++ ePaperDisplay.py
@@ -4,8 +4,8 @@ import sys
 import os
 #picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 #libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-picdir = '/pic'
-libdir = '/lib'
+picdir = '/Python/ePaperDisplay/pic'
+libdir = '/Python/ePaperDisplay/lib'
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
