ccd87e29ed0feb9412d9cba84871fccb9c61dd54
Jay Dwight <jcdwight@gmail.com>
2023-06-13 17:57:09 -0400

    Combined

--- ePaperDisplay.py
+++ ePaperDisplay.py
@@ -1,50 +1,131 @@
 ﻿#!/usr/bin/python
 # -*- coding:utf-8 -*-
+from __future__ import print_function
+
 import sys
 import os
-#picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-#libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+import datetime
+import os.path
+import logging
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+import platform as plat
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+def CheckPlatform():
+    #Checks the platform the program is running on.  Linux = 1, Windows = 2, everything else is 3
+    if (plat.platform()[0] == "L" or plat.platform()[0] == "l"):
+        return 1
+    elif (plat.platform()[0] == "W" or plat.platform()[0] == "w"):
+        return 2
+    else:
+        return 3
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+
+
 picdir = 'pic'
 libdir = 'lib'
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
-import logging
-from waveshare_epd import epd7in5_V2
-import time
-from PIL import Image,ImageDraw,ImageFont
-import traceback
+print('got to check platform')
 
-logging.basicConfig(level=logging.DEBUG)
+if (CheckPlatform() == 1):
+    from waveshare_epd import epd7in5_V2
+elif (CheckPlatform() == 2):
+    pass
 
-try:
-    logging.info("epd7in5_V2 Demo")
-    epd = epd7in5_V2.EPD()
+def ePaperDemo():
+
+    try:
+        logging.info("epd7in5_V2 Demo")
+        epd = epd7in5_V2.EPD()
     
-    logging.info("init and Clear")
-    epd.init()
-    epd.Clear()
-    font96 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 96)
-    font48 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 48)
-    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
+        logging.info("init and Clear")
+        epd.init()
+        epd.Clear()
+        font96 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 96)
+        font48 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 48)
+        font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+        font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
 
-    Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    draw = ImageDraw.Draw(Limage)
+        Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+        draw = ImageDraw.Draw(Limage)
 
-    draw.text((10,650), 'WELCOME', font = font96, fill = 0)
-    draw.text((100,375), 'BACK!', font = font96, fill = 0)
+        draw.text((10,650), 'WELCOME', font = font96, fill = 0)
+        draw.text((100,375), 'BACK!', font = font96, fill = 0)
     
-    epd.display(epd.getbuffer(Limage))
-    time.sleep(2)
+        epd.display(epd.getbuffer(Limage))
+        time.sleep(2)
 
-    logging.info("Goto Sleep...")
-    epd.sleep()
+        logging.info("Goto Sleep...")
+        epd.sleep()
     
-except IOError as e:
-    logging.info(e)
+    except IOError as e:
+        logging.info(e)
     
-except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
-    epd7in5_V2.epdconfig.module_exit()
-    exit()
+    except KeyboardInterrupt:    
+        logging.info("ctrl + c:")
+        epd7in5_V2.epdconfig.module_exit()
+        exit()
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+            return
+
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+if __name__ == '__main__':
+    main()
