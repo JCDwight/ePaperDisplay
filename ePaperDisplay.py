b9efa42a7ce1d9f0c4b0cd573a8ef6e853313968
Jay Dwight <jcdwight@gmail.com>
2023-06-15 21:24:48 -0400

    S

--- ePaperDisplay.py
+++ ePaperDisplay.py
@@ -59,7 +59,7 @@ def ePaperDemo():
 
     try:
         logging.info("epd7in5b_V2 Demo")
-        epd = epd7in5_V2.EPD()
+        epd = epd7in5b_V2.EPD()
     
         logging.info("init and Clear")
         epd.init()
