d326eab250810f68f4b63b013a57594dfca3ec18
Jay Dwight <jcdwight@gmail.com>
2023-06-16 00:18:34 -0400

    fsd

--- ePaperDisplay.py
+++ ePaperDisplay.py
@@ -117,12 +117,12 @@ def ePaperDemo():
 
         #draw.rectangle((1,1,479,275))
         draw.rectangle((0,0,480,275),width = 5, outline= 0)
-        #start = events[0]['start'].get('dateTime', events[0]['start'].get('date'))
-        #date = datetime.strptime(start, "%Y-%m-%d")
-        #formatted = date.strftime("%a, (%b, %d) - ")
-        #formatted2 = str(events[0]['summary'])
-        formatted = 'Test 1'
-        formatted2 = 'Test 2'
+        start = events[0]['start'].get('date')
+        date = datetime.strptime(start, "%Y-%m-%d")
+        formatted = date.strftime("%a, (%b, %d) - ")
+        formatted2 = str(events[0]['summary'])
+        #formatted = 'Test 1'
+        #formatted2 = 'Test 2'
         draw.text((20, 20), formatted, font = font48, fill = 0)
         draw.text((100, 120), formatted2, font = font48, fill = 0)
         draw.rectangle((0,275,480,550),width = 5, outline= 0)        
